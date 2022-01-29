a230fd34ce1606b646ad37c33c7d77e4ff62ee5f
Janos <jfazakas@gmail.com>
2022-01-29 13:28:04 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+import constants from "../constants";
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          {constants.navbar.title.en}
+        </Typography>
+        <Button color="inherit">{constants.navbar.home.en}</Button>
+        <Button color="inherit">{constants.navbar.services.en}</Button>
+        <Button color="inherit">{constants.navbar.contact.en}</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default Navbar;
---
+++ src/constants.js
@@ -0,0 +1,18 @@
+const constants = {
+  navbar: {
+    title: {
+      en: "ZitromSoft",
+    },
+    contact: {
+      en: "Contact",
+    },
+    services: {
+      en: "Services",
+    },
+    home: {
+      en: "Home",
+    },
+  },
+};
+
+export default constants;
